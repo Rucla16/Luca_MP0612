3152dbdf31ccc2f8d77e387e48fead773d08ced4
Luca <lopritescu@institutmvm.cat>
2026-02-22 12:17:55 +0100

    Add files via upload

---
+++ transformador.js
@@ -0,0 +1,8 @@
+function transformar(minuts, hores, numero) {
+    numero = 300;
+    minuts = numero / 60;
+    hores = numero / 3600;
+    console.log("Número transformat en minuts: " , minuts, "i hores: " , hores);
+}
+
+transformar();
